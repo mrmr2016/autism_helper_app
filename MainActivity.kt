0538568d604df2d3bd635fd36c5f9cd542c9ebba
mrmr2016 <maria.alhabeeb@gmail.com>
2024-12-08 20:43:57 +0100

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.autismhelperapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // إضافة زر لفتح الشاشة الجديدة
+        val openNewActivityButton: Button = findViewById(R.id.openNewActivityButton)
+        openNewActivityButton.setOnClickListener {
+            val intent = Intent(this, NewActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
